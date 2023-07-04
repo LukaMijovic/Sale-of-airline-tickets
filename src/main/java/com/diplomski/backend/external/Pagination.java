fc52134e3bf0370ae4cd76c6dd9c50c978f2efe9
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-07-04 20:34:38 +0200

    Added attributes to class PAgination. Implemented class PaginationService

--- src/main/java/com/diplomski/backend/external/Pagination.java
+++ src/main/java/com/diplomski/backend/external/Pagination.java
@@ -1,4 +1,26 @@
 package com.diplomski.backend.external;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pagination")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pagination {
+    private Long id;
+    @Column(name = "calling_time")
+    private LocalDateTime callingTime;
+    private int limit;
+    private int offset;
+    private int count;
+    private int total;
+    private String message;
 }
--- src/main/java/com/diplomski/backend/external/service_external/PaginationService.java
+++ src/main/java/com/diplomski/backend/external/service_external/PaginationService.java
@@ -1,12 +1,16 @@
 package com.diplomski.backend.external.service_external;
 
 import com.diplomski.backend.external.Pagination;
+import com.diplomski.backend.repository.PaginationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaginationService {
+    @Autowired
+    private PaginationRepository paginationRepository;
 
     public void savePagination(Pagination pagination){
-
+        paginationRepository.save(pagination);
     }
 }
