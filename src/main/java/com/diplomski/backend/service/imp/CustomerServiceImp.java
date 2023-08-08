94168d6c8c5cb0e7388a9c346d3eef9a62f2e95a
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-08-08 18:01:32 +0200

    Implemented functionality for registration new customer

--- src/main/java/com/diplomski/backend/service/imp/CustomerServiceImp.java
+++ src/main/java/com/diplomski/backend/service/imp/CustomerServiceImp.java
@@ -2,6 +2,7 @@ package com.diplomski.backend.service.imp;
 
 import com.diplomski.backend.domain.Customer;
 import com.diplomski.backend.domain.enumeration.AccountStatus;
+import com.diplomski.backend.domain.enumeration.Role;
 import com.diplomski.backend.repository.CustomerRepository;
 import com.diplomski.backend.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ public class CustomerServiceImp implements CustomerService {
     @Override
     public Customer registration(Customer customer) {
         customer.setAccountStatus(AccountStatus.ACTIVE);
+        customer.setRole(Role.CUSTOMER);
         customer.setRegistrationTime(LocalDateTime.now());
         return customerRepository.save(customer);
     }
