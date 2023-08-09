19540c723462e6324a890abaadf4cc4e0ee8567f
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-08-09 18:33:51 +0200

    Changed DTO Object for FlightActive. Added iataCode for airports.

---
+++ src/main/java/com/diplomski/backend/dto/FlightDTOActive2.java
@@ -0,0 +1,20 @@
+package com.diplomski.backend.dto;
+
+import com.diplomski.backend.domain.enumeration.FlightStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record FlightDTOActive2(
+        Long id,
+        LocalDate flightDate,
+        LocalDateTime departureScheduled,
+        LocalDateTime arrivalScheduled,
+        String airlineName,
+        String airlineImage,
+        String iataCodeDep,
+        String airportCityDep,
+        String iataCodeArr,
+        String airportCityArr
+) {
+}
