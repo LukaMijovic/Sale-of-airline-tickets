3a279ebe3c0d4316d9ab3c4e2751ed5216710db5
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-08-11 15:35:56 +0200

    Changed some functionality for cancel Booking

--- src/main/java/com/diplomski/backend/repository/SeatStatusRepository.java
+++ src/main/java/com/diplomski/backend/repository/SeatStatusRepository.java
@@ -1,9 +1,13 @@
 package com.diplomski.backend.repository;
 
 import com.diplomski.backend.domain.SeatStatus;
+import com.diplomski.backend.domain.enumeration.SeatStatusEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SeatStatusRepository extends JpaRepository<SeatStatus,Long> {
+    Optional<SeatStatus> findBySeatStatusAndFlightIdAndSeatId(SeatStatusEnum opened, Long flightId, Long seatId);
 }
