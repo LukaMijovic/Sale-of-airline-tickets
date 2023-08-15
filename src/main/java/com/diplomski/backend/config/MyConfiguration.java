687e40a399cb5d6bb1c2374f613163ff4b703549
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-08-15 13:16:35 +0200

    Created configuration classes for security

--- src/main/java/com/diplomski/backend/config/MyConfiguration.java
+++ src/main/java/com/diplomski/backend/config/MyConfiguration.java
@@ -1,10 +1,19 @@
 package com.diplomski.backend.config;
 
+import com.diplomski.backend.exception.NoSuchElementFoundException;
 import com.diplomski.backend.external.MyUriBuilder;
+import com.diplomski.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class MyConfiguration {
@@ -16,6 +25,31 @@ public class MyConfiguration {
     @Bean
     public MyUriBuilder myUriBuilder(){
         return new MyUriBuilder();
+    }
+
+    @Autowired
+    private UserRepository repository;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email -> repository.findByEmailNew(email)
+                .orElseThrow(() -> new NoSuchElementFoundException("User not found"));
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
--- src/main/java/com/diplomski/backend/repository/UserRepository.java
+++ src/main/java/com/diplomski/backend/repository/UserRepository.java
@@ -2,8 +2,14 @@ package com.diplomski.backend.repository;
 
 import com.diplomski.backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    UserDetails findByEmail(String email);
+
+    Optional<UserDetails> findByEmailNew(String email);
 }
